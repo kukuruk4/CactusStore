009ee8ba1f3fd31cde5ec6c25a808db869292c32
kukuruk4 <imanzhikov1999@icloud.com>
2021-04-19 19:55:22 +0300

    API added

---
+++ CactusStore.API/Controllers/CactusController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using CactusStore.BLL.DTO;
+using CactusStore.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CactusStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CactusController : ControllerBase
+    {
+        IOrderService orderService;
+        public CactusController(IOrderService serv)
+        {
+            orderService = serv;
+        }
+
+        [HttpGet]
+        public IEnumerable<CactusDTO> Get()
+        {
+            return orderService.GetCactuses();
+        }
+
+        [HttpGet("{id}")]
+        public CactusDTO Get(int id)
+        {
+            return orderService.GetCactus(id);
+        }
+    }
+    
+}
---
+++ CactusStore.API/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CactusStore.BLL.DTO;
+using CactusStore.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace CactusStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        IOrderService orderService;
+        public OrderController(IOrderService serv)
+        {
+            orderService = serv;
+        }
+
+        // POST api/order
+        [HttpPost]
+        public void Post([FromBody] OrderDTO newOrder)
+        {
+            orderService.MakeOrder(newOrder);
+        }
+    }
+}
